2dcd9abd1a28a2f23655666b0e2ddcb16de08688
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-01-25 23:45:04 +0530

    Update app.js

--- Guess-My-Number/app.js
+++ Guess-My-Number/app.js
@@ -4,3 +4,6 @@
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent= 'Correct Number'
+
+document.querySelector('.number').textContent=13
+document.querySelector('.score').textContent=10
