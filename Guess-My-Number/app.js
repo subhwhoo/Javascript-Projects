68d122173119133af80a8a0c6e8872bcb474f1dd
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-01-27 00:08:11 +0530

    Refactored App.js

--- Guess-My-Number/app.js
+++ Guess-My-Number/app.js
@@ -1,36 +1,35 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
+
   //When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "Please Enter a Number";
-  } 
+  }
+
   //When Player Wins the Game
   else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
 
-  }
-  //When the guess is Higher 
-  else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too High";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game";
-      document.querySelector(".score").textContent = 0;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
     }
-  } 
-  //When the guess is lower
-  else if (guess < secretNumber) {
+  }
+  //When the Guess is wrong
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too Low";
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "Too High" : "Too Low";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
@@ -38,4 +37,15 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start Guessing Again";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
