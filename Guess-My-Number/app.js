ecb52d1ed40dcccbd995d8fa5ac48782e08e4754
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-01-26 20:54:19 +0530

    Game Logic Created

--- Guess-My-Number/app.js
+++ Guess-My-Number/app.js
@@ -1,8 +1,41 @@
-'use strict';
+"use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+let score = 20;
 
-console.log(document.querySelector('.message').textContent);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  //When there is no input
+  if (!guess) {
+    document.querySelector(".message").textContent = "Please Enter a Number";
+  } 
+  //When Player Wins the Game
+  else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
 
-document.querySelector('.message').textContent= 'Correct Number'
-
-document.querySelector('.number').textContent=13
+  }
+  //When the guess is Higher 
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } 
+  //When the guess is lower
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
