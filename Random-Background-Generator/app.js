a54b6245f7fdd84e97d1d1c459dd7599d03e0b76
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-03 10:26:33 +0530

    Added App.js Functionality
    Completed App except Hex Code

---
+++ Random-Background-Generator/app.js
@@ -0,0 +1,24 @@
+function changeColor1() {
+    document.getElementById('p1').style.backgroundColor =
+        'rgb(' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) + ')'
+}
+function changeColor2() {
+    document.getElementById('p2').style.backgroundColor =
+        'rgb(' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) + ')'
+}
+function changeColor3() {
+    document.getElementById('p3').style.backgroundColor =
+        'rgb(' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) + ')'
+}
+function changeColor4() {
+    document.getElementById('p4').style.backgroundColor =
+        'rgb(' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) +
+        ',' + Math.round(Math.random() * 255) + ')'
+}
