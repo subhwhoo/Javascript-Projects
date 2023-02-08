3fd72341c768b7683af5ee762a64a4447a9a318b
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-08 22:00:34 +0530

    Script Updated

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,2 +1,24 @@
+const screenDisplay= document.querySelector('.screen')
 const buttons=document.querySelectorAll('button')
 
+let calculation=[]
+let accumulativeCalculation
+function calculate(button) {
+    const value= button.textContent
+
+    if (value === "Clear") {
+        calculation=[]
+        screenDisplay.textContent='.'
+    }
+    else if (value === "=") {
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    }
+    else {
+        calculation.push(value)
+        accumulativeCalculation=calculation.join('')
+        screenDisplay.textContent=accumulativeCalculation
+    }
+
+}
+
+buttons.forEach(button=>button.addEventListener( 'click',()=>calculate(button)))
