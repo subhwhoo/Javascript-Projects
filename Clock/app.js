77921fbcb4a6a8c0218346ee99007976233f8ff1
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-11 00:54:52 +0530

    Completed Clock

---
+++ Clock/app.js
@@ -0,0 +1,20 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+
+console.log(hourEl, minuteEl, secondEl);
+
+function updateClock(){
+    const currentDate = new Date()
+    const hours = currentDate.getHours()
+    const minutes = currentDate.getMinutes()
+    const seconds = currentDate.getSeconds()
+    const hourDeg = (hours/12)*360
+    const minuteDeg = (minutes/60)*360
+    const secondDeg = (seconds/60)*360
+    hourEl.style.transform=`rotate(${hourDeg}deg)`
+    minuteEl.style.transform=`rotate(${minuteDeg}deg)`
+    secondEl.style.transform=`rotate(${secondDeg}deg)`
+}
+
+setInterval(updateClock, 1000)
