e2aaff2266fbe322201deb2a3dbceae3c63a8645
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-12 21:49:32 +0530

    Testimonial Slider

--- Testimonial-Slider/app.js
+++ Testimonial-Slider/app.js
@@ -2,20 +2,20 @@ const testimonials = [
   {
     name: "John Doe",
     photoURL:
-      "https//images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=2000&q=60",
+      "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfDF8MHx8&auto=format&fit=crop&w=2000&q=60",
     text: "I didn't even need training. Keep up the excellent work. Absolutely wonderful!",
   },
   {
-    name: "John Doe",
+    name: "Abc Xyx",
     photoURL:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=2000&q=60",
-    text: "2nd Text",
+    text: "Consectetur adipiscing elit, sed do eiusmod tempor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.",
   },
   {
-    name: "John Doe2",
+    name: "Jane Joe",
     photoURL:
       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=2000&q=60",
-    text: "3rd Text",
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.",
   },
 ];
 const imgEl = document.querySelector("img");
